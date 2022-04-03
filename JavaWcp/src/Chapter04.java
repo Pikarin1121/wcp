1e92f90031847a4fdfd91c08fa3a62c09ad482c4
Pikarin1121 <96364635+Pikarin1121@users.noreply.github.com>
2022-04-03 23:15:01 +0900

    Javaカリキュラム5章まで

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name1 = "千葉　暉";
+		System.out.println(name1);
+		
+		name1 = "安倍晴明";
+		System.out.println(name1);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 4370748375l;
+		double double1 = 4779.34;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 0.353f;
+		Integer int1 = 3;
+		Character char1 = 'ひ';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(char1);
+		
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+}
