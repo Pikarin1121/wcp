cbe1bcd214b19c2b8bb9a010c411f248bc74dedc
Pikarin1121 <96364635+Pikarin1121@users.noreply.github.com>
2022-04-18 22:49:41 +0900

    Javaカリキュラム15章

---
+++ JavaWcp/src/practice/Chapter15_2.java
@@ -0,0 +1,10 @@
+package practice;
+
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+
+public class Chapter15_2 {
+	public static void main(String[] args) {
+		FileSystem fs = FileSystems.getDefault();
+	}
+}
